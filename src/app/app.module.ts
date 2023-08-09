68b4abc223e757c118961eb979fbcd0537c6c87f
Martyna <martyna.marszalek@talan.com>
2023-08-09 15:12:30 +0200

    not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TagsComponent } from './tags/tags.component';
 import { BookPageComponent } from './book-page/book-page.component';
 import { FooterComponent } from './footer/footer.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     TagsComponent,
     BookPageComponent,
     FooterComponent,
-    CartPageComponent
+    CartPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
